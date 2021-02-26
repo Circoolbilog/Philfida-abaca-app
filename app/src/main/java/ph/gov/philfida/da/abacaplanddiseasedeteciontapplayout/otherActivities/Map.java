aefa82c5c069c519dc1c560da4037c725df3aa18
Circoolbilog <clarkllarena@ymail.com>
2021-02-27 01:10:06 +0800

    Added Map using openStreetMap

--- app/src/main/java/ph/gov/philfida/da/abacaplanddiseasedeteciontapplayout/otherActivities/Map.java
+++ app/src/main/java/ph/gov/philfida/da/abacaplanddiseasedeteciontapplayout/otherActivities/Map.java
@@ -29,7 +29,7 @@ public class Map extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_map);
 
         Context ctx = this.getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));

d0ed15c24e30372d5ff3f6ce2d28db8ddf2b7c54
Circoolbilog <clarkllarena@ymail.com>
2021-03-09 10:54:04 +0800

    Added Map using openStreetMap

--- app/src/main/java/ph/gov/philfida/da/abacaplanddiseasedeteciontapplayout/otherActivities/Map.java
+++ app/src/main/java/ph/gov/philfida/da/abacaplanddiseasedeteciontapplayout/otherActivities/Map.java
@@ -1,5 +1,6 @@
 package ph.gov.philfida.da.abacaplanddiseasedeteciontapplayout.otherActivities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -8,8 +9,17 @@ import ph.gov.philfida.da.abacaplanddiseasedeteciontapplayout.R;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
@@ -18,13 +28,21 @@ import org.osmdroid.views.CustomZoomButtonsController;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.util.ArrayList;
+import java.util.Locale;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class Map extends AppCompatActivity {
     private MapView map = null;
-
     private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
+    private FusedLocationProviderClient fusedLocationClient;
+    double latitude;
+    double longitude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +51,8 @@ public class Map extends AppCompatActivity {
 
         Context ctx = this.getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         map = findViewById(R.id.mapview);
         map.setTileSource(TileSourceFactory.MAPNIK);
@@ -45,19 +65,44 @@ public class Map extends AppCompatActivity {
         map.setMultiTouchControls(true);
 
 
+        getCurrentLocation();
         CompassOverlay compassOverlay = new CompassOverlay(this, map);
         compassOverlay.enableCompass();
         map.getOverlays().add(compassOverlay);
 
-        GeoPoint point = new GeoPoint(45.845557, 26.170010);
 
-        Marker startMarker = new Marker(map);
+        GeoPoint point = new GeoPoint(13.1717, 123.2940);
+
+        Marker startMarker = new Marker(map );
         startMarker.setPosition(point);
         startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER);
         map.getOverlays().add(startMarker);
 
         map.getController().setCenter(point);
     }
+
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions(Map.this,new String[]{ACCESS_FINE_LOCATION},44);
+            return;
+        }
+        fusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+                    // Logic to handle location object
+                }
+                else {
+                    longitude=0.0;
+                    latitude=0.0;
+                }
+            }
+        });
+        Toast.makeText(this, "long/Lat"+longitude+"/"+latitude, Toast.LENGTH_SHORT).show();
+    }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {

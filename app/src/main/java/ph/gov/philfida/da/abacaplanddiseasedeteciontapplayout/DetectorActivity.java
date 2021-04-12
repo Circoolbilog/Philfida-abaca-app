9089307e48fc45e23a305fbfde816899d9df185e
Circoolbilog <clarkllarena@ymail.com>
2021-04-12 11:47:54 +0800

    Added new model2.tflite

--- app/src/main/java/ph/gov/philfida/da/abacaplanddiseasedeteciontapplayout/DetectorActivity.java
+++ app/src/main/java/ph/gov/philfida/da/abacaplanddiseasedeteciontapplayout/DetectorActivity.java
@@ -59,11 +59,11 @@ public class DetectorActivity extends Diagnose implements OnImageAvailableListen
     // Configuration values for the prepackaged SSD model.
     private static final int TF_OD_API_INPUT_SIZE = 640;
     private static final boolean TF_OD_API_IS_QUANTIZED = false;
-    private static final String TF_OD_API_MODEL_FILE = "model9.tflite";
-    private static final String TF_OD_API_LABELS_FILE = "labelmap1.pbtxt";
+    private static final String TF_OD_API_MODEL_FILE = "model21.tflite";
+    private static final String TF_OD_API_LABELS_FILE = "class_beta2.txt";
     private static final DetectorMode MODE = DetectorMode.TF_OD_API;
     // Minimum detection confidence to track a detection.
-    private static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.5f;
+    private static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.3f;
     private static final boolean MAINTAIN_ASPECT = false;
     private static final Size DESIRED_PREVIEW_SIZE = new Size(640, 480);
     private static final boolean SAVE_PREVIEW_BITMAP = false;
--- app/src/main/java/ph/gov/philfida/da/abacaplanddiseasedeteciontapplayout/containers/SettingsContainer.java
+++ app/src/main/java/ph/gov/philfida/da/abacaplanddiseasedeteciontapplayout/containers/SettingsContainer.java
@@ -1,7 +1,23 @@
 package ph.gov.philfida.da.abacaplanddiseasedeteciontapplayout.containers;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
 
 public class SettingsContainer extends Application {
-    int diagnoseMode = 0;
+    public static final String SHARED_PREFS = "sharedPrefs";
+
+    public static final String DIAGNOSE_MODE = "diagnoseMode";
+    int diagnoseMode;
+
+//    SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+
+    public int getDiagnoseMode() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        diagnoseMode = sharedPreferences.getInt(DIAGNOSE_MODE, 0);
+        return diagnoseMode;
+    }
+    public void setDiagnoseMode(int diagnoseMode) {
+        this.diagnoseMode = diagnoseMode;
+    }
 }

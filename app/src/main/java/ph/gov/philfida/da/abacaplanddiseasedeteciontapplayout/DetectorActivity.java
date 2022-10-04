a7acc7db4fc46424890d319daac7b78fc7fcb4f0
Clark Llarena <clarkllarena@ymail.com>
2022-10-04 14:13:26 +0800

    Addedd new model

--- app/src/main/java/ph/gov/philfida/da/abacaplanddiseasedeteciontapplayout/DetectorActivity.java
+++ app/src/main/java/ph/gov/philfida/da/abacaplanddiseasedeteciontapplayout/DetectorActivity.java
@@ -76,7 +76,7 @@ public class DetectorActivity extends Diagnose implements OnImageAvailableListen
     // Configuration values for the prepackaged SSD model.
     private static final int TF_OD_API_INPUT_SIZE = 640;
     private static final boolean TF_OD_API_IS_QUANTIZED = false;
-    private static final String TF_OD_API_MODEL_FILE = "hpc_model2.tflite";
+    private static final String TF_OD_API_MODEL_FILE = "office_model16.tflite";
     private static final String TF_OD_API_LABELS_FILE = "symptoms.txt";
     private static final DetectorMode MODE = DetectorMode.TF_OD_API;
     // Minimum detection confidence to track a detection.
